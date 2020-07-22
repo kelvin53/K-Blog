9e59ccf592e55248a6be9e54e646b17c4c25d0c0
kelvin53 <geshkoskei53@gmail.com>
2020-07-22 22:25:37 +0300

    edit config module

--- config.py
+++ config.py
@@ -4,20 +4,20 @@ from dotenv import load_dotenv
 load_dotenv()
 class Config:
     Debug = True
-    SECRET_KEY= os.environ.get('SECRET_KEY')
+    SECRET_KEY= '4444'
     UPLOADED_PHOTOS_DEST = 'app/static/images'
     MAIL_SERVER = 'smtp.gmail.com'
     MAIL_PORT = 465
     MAIL_USE_TLS = False
     MAIL_USE_SSL = True
-    MAIL_USERNAME= os.environ.get('MAIL_USERNAME')
-    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    MAIL_USERNAME='ijanemercy@gmail.com'
+    MAIL_PASSWORD = '@janeMercy700'
 
 
 class DevConfig(Config):
     Debug = True
-    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://joozao:12345@localhost/blogyu'
 
 class ProdConfig(Config):
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://joozao:12345@localhost/blogyu'
 configurations = {"development":DevConfig, "production":ProdConfig}
